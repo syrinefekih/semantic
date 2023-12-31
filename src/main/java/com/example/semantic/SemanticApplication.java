52340e2df1f1aff00268495bb1b0f20f376a1783
Syrine Fekih <syrine.fekih@etudiant-enit.utm.tn>
2023-12-31 14:42:04 +0100

    api

--- src/main/java/com/example/semantic/SemanticApplication.java
+++ src/main/java/com/example/semantic/SemanticApplication.java
@@ -10,14 +10,5 @@ public class SemanticApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SemanticApplication.class, args);
-        try {
-            String userInput = "I want to visit paris and London and Tunisia";
-            String spotlightResponse = SpotlightEntityRecognition.recognizeEntities(userInput);
-            System.out.println("DBpedia Spotlight Response: " + spotlightResponse);
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
-}
+    }
