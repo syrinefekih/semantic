1f2957686333a6d69026c5297a7ec1a6f24bc600
kallelfarah <farah.kallel@etudiant-enit.utm.tn>
2024-01-15 09:44:11 +0100

    neww

--- src/main/java/com/example/semantic/SemanticController.java
+++ src/main/java/com/example/semantic/SemanticController.java
@@ -1,15 +1,14 @@
 package com.example.semantic;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
 @RestController
 @RequestMapping("/semantic")
+@CrossOrigin(origins = "http://localhost:4200")
+
 class SemanticController {
 
     @PostMapping("/processInput")
